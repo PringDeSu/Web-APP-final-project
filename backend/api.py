64955d336a037ac595cf3911844d65f3cbb08f30
pingchungchang <pingchungchang@gmail.com>
2025-05-23 23:22:46 +0800

    added branch, packed with gunicorn(but somehow the overhead is big sometimes). Changed API variable names and interactions GREATLY

--- backend/api.py
+++ backend/api.py
@@ -9,92 +9,97 @@ import argparse
 import logging
 from logging.handlers import RotatingFileHandler
 import feature_extractor
-
-# parse arguments
-parser = argparse.ArgumentParser()
-parser.add_argument('--tmp-folder', type=str, default='./files')
-parser.add_argument('--log-folder', type=str, default='./log')
-parser.add_argument('--output-folder', type=str, default='./output')
-parser.add_argument('--cache-folder', type=str, default='./cache')
-parser.add_argument('--port',		type=int, default='3000')
-
-args = parser.parse_args()
-
 # main part
 app = Flask(__name__)
 CORS(app)  # This enables CORS for all routes and origins
 
-# setting up log files
-os.makedirs(args.log_folder, exist_ok=True)
-log_file = os.path.join(args.log_folder, 'api.log')
-handler = RotatingFileHandler(log_file, maxBytes=1048576, backupCount=5)
-handler.setLevel(logging.INFO)
-formatter = logging.Formatter(
-	'[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
-)
-handler.setFormatter(formatter)
+def init_folders(input_folder, output_folder, cache_folder, log_folder):
+    # setting up log files
+    os.makedirs(log_folder, exist_ok=True)
+    log_file = os.path.join(log_folder, 'api.log')
+    handler = RotatingFileHandler(log_file, maxBytes=1048576, backupCount=5)
+    handler.setLevel(logging.INFO)
+    formatter = logging.Formatter(
+        '[%(asctime)s] %(levelname)s in %(module)s: %(message)s'
+    )
+    handler.setFormatter(formatter)
 
-app.logger.addHandler(handler)
-app.logger.setLevel(logging.INFO)
+    app.logger.addHandler(handler)
+    app.logger.setLevel(logging.INFO)
 
-UPLOAD_FOLDER = args.tmp_folder
-os.makedirs(UPLOAD_FOLDER, exist_ok=True)
-app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+    os.makedirs(input_folder, exist_ok=True)
+    app.config['INPUT_FOLDER'] = input_folder
 
-OUTPUT_FOLDER = args.output_folder
-os.makedirs(OUTPUT_FOLDER, exist_ok=True)
-app.config['OUTPUT_FOLDER'] = OUTPUT_FOLDER
+    os.makedirs(output_folder, exist_ok=True)
+    app.config['OUTPUT_FOLDER'] = output_folder
 
-CACHE_FOLDER = args.cache_folder
-os.makedirs(CACHE_FOLDER, exist_ok=True)
-app.config['CACHE_FOLDER'] = CACHE_FOLDER
+    os.makedirs(cache_folder, exist_ok=True)
+    app.config['CACHE_FOLDER'] = cache_folder
+
 
 @app.route('/api/upload/<path:filename>')
 def serve_file(filename):
-    file_path = os.path.join(OUTPUT_FOLDER, filename)
+    file_path = os.path.join(app.config['OUTPUT_FOLDER'], filename)
     if os.path.isfile(file_path):
-        return send_from_directory(OUTPUT_FOLDER, filename, as_attachment=False)
+        return send_from_directory(app.config['OUTPUT_FOLDER'], filename, as_attachment=False)
     else:
         abort(404, description="File not found")
 
 
 @app.route('/api/post', methods=['POST'])
 def upload_file():
-	if 'file' not in request.files:
-		print('No file part in the request.')
-		return jsonify({'error': 'No file part'}), 400
+    if 'file' not in request.files:
+        print('No file part in the request.')
+        return jsonify({'error': 'No file part'}), 400
 
-	file = request.files['file']
+    file = request.files['file']
 
-	if file.filename == '':
-		print('No file selected.')
-		return jsonify({'error': 'No file selected'}), 400
+    if file.filename == '':
+        print('No file selected.')
+        return jsonify({'error': 'No file selected'}), 400
 
-	filename = secure_filename(file.filename)
-	timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
-	unique_filename = f"{timestamp}_{filename}"
-	file_path = os.path.join(app.config['UPLOAD_FOLDER'], unique_filename)
-	file.save(file_path)
+    filename = secure_filename(file.filename)
+    timestamp = datetime.now().strftime('%Y%m%d%H%M%S')
+    unique_filename = f"{timestamp}_{filename}"
+    file_path = os.path.join(app.config['INPUT_FOLDER'], unique_filename)
+    file.save(file_path)
 
-	app.logger.info(f"Received file: {file.filename}")
-	app.logger.info(f"Stored as: {unique_filename}")
-	app.logger.info(f"Saved in: {file_path}")
+    app.logger.info(f"Received file: {file.filename}")
+    app.logger.info(f"Stored as: {unique_filename}")
+    app.logger.info(f"Saved in: {file_path}")
 
-	# parsing the file
-	features, stored_name = feature_extractor.extract_from_file(file_path, app.config['OUTPUT_FOLDER'], app.config['CACHE_FOLDER'])
+    # parsing the file
+    features, stored_name = feature_extractor.extract_from_file(file_path, app.config['OUTPUT_FOLDER'], app.config['CACHE_FOLDER'])
 
-	# return the attributes you parsed
-	# modify this part
-	return jsonify({
-		'message': 'File uploaded successfully',
-		'fileInfo': {
-			'originalName': file.filename,
-			'originalPath': file_path,
-			'outputPath': f'{OUTPUT_FOLDER}/{stored_name}',
-			'outputName': stored_name,
-		},
-		'features': features
-	})
+    # return the attributes you parsed
+    # modify this part
+    output_path = app.config['OUTPUT_FOLDER']+f'/{stored_name}'
+    api_path = f'/api/upload/{stored_name}'
+    return jsonify({
+        'message': 'File uploaded successfully',
+        'fileInfo': {
+            'originalName': file.filename,
+            'originalPath': file_path,
+            'outputPath': output_path,
+            'outputName': stored_name,
+            'apiPath'   : api_path,
+        },
+        'features': features
+    })
 
 if __name__ == '__main__':
-	app.run(host='0.0.0.0', debug=True, port=args.port)
+
+    # parse arguments
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--input-folder', type=str, default='./input')
+    parser.add_argument('--log-folder', type=str, default='./log')
+    parser.add_argument('--output-folder', type=str, default='./output')
+    parser.add_argument('--cache-folder', type=str, default='./cache')
+    parser.add_argument('--port',       type=int, default='3000')
+
+    args = parser.parse_args()
+
+    init_folders(args.input_folder, args.output_folder, args.cache_folder, args.log_folder)
+    app.run(host='0.0.0.0', debug=False, port=args.port)
+else:
+    init_folders(os.getenv("INPUT_FOLDER", "./input"), os.getenv("OUTPUT_FOLDER", "./output"), os.getenv("CACHE_FOLDER", "./cache"), os.getenv("LOG_FOLDER", "./log"))
