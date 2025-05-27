d49f1d7c876daa0c4663a657e690ac01835a3038
PringDeSu <ianneeddiamond@gmail.com>
2025-05-27 22:18:04 +0800

    connected Nginx and backend

--- nginx/start.sh
+++ nginx/start.sh
@@ -1,1 +1,1 @@
-envsubst '${WEB_SERVER_FE_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf && nginx -g "daemon off;"
+envsubst '${WEB_SERVER_FE_PORT} ${WEB_SERVER_BE_PORT} ${BACKEND_SERVER_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf && nginx -g "daemon off;"
